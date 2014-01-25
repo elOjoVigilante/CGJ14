b979b728863b78aa88062424f666a4c2a3fe761b
Firebeam <nulike@gmail.com>
2014-01-25 15:10:20 -0430

    Inicio de trabajo en el menú

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			//fade intro
+			FadeMusic(4);
+
+			//load
+			Application.LoadLevel("Scene1");
+		}
+	}
+
+	private void FadeMusic(float seconds) {
+		float initialVolume = audio.volume;
+		float changingVolume;
+		float t = 0f;
+
+		while (t < 1) {
+			t += Time.deltaTime / seconds;
+			changingVolume = Mathf.Lerp(initialVolume, 0, t);
+			audio.volume = changingVolume;
+		}
+	}
+}
