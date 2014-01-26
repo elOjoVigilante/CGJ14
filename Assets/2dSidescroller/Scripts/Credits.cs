aa217c75a4047aa9ff71010152f606c6712afd0f
Firebeam <nulike@gmail.com>
2014-01-26 02:36:36 -0430

    Créditos!

--- Assets/2dSidescroller/Scripts/Credits.cs
+++ Assets/2dSidescroller/Scripts/Credits.cs
@@ -6,7 +6,11 @@ public class Credits : MonoBehaviour {
 	float fadeTime = 4.0f;
 	
 	GameObject gameCamera;
+	GameObject menuObject;
 	AudioSource music;
+
+	Vector3 creditsPosition;
+	float scrollSpeed = 3.5f;
 	
 	// Use this for initialization
 	void Start () {
@@ -14,10 +18,22 @@ public class Credits : MonoBehaviour {
 		music = gameCamera.GetComponent<AudioSource> ();
 		music.enabled = true;
 		music.volume = 1.0f;
+
+		menuObject = GameObject.Find ("Menu");
+		creditsPosition = menuObject.transform.position;
+		creditsPosition.y = -2.0f;
+		menuObject.transform.position = creditsPosition;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (creditsPosition.y <= 2.0f) {
+			creditsPosition.y += Time.deltaTime / scrollSpeed;
+		} else {
+			creditsPosition.y = -2.0f;
+		}
+		menuObject.transform.position = creditsPosition;
+
 		if (Input.GetKeyDown (KeyCode.Escape)) {
 			//fade intro
 			StartCoroutine(FadeMusic());
