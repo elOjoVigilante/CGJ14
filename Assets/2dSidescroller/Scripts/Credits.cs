c10aa4dc2fc818a51013dcdb8a29d1ed2da334d6
Firebeam <nulike@gmail.com>
2014-01-26 01:15:38 -0430

    Pantalla de creditos Boceto

---
+++ Assets/2dSidescroller/Scripts/Credits.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+
+	float fadeTime = 4.0f;
+	
+	GameObject gameCamera;
+	AudioSource music;
+	
+	// Use this for initialization
+	void Start () {
+		gameCamera = GameObject.Find ("Main Camera");
+		music = gameCamera.GetComponent<AudioSource> ();
+		music.enabled = true;
+		music.volume = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			//fade intro
+			StartCoroutine(FadeMusic());
+		}
+	}
+	
+	//si alguien hace que esto funcione me avisa >_>
+	private IEnumerator FadeMusic() {
+		while (music.volume > 0) {
+			music.volume -= Time.deltaTime / fadeTime;
+			yield return null;
+		}
+		//load
+		Application.Quit ();
+	}
+}
